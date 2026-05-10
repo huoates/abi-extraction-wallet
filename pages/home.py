bde9d5f1861c72090d551c63adab3be624ba2a91
huoates <huoates@gmail.com>
2026-05-10 01:41:22 -0500

    Add home page content

---
+++ pages/home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Welcome to Koen Bee")
+st.write("Welcome to the Home page! Some features of the site include:")
+st.write("- View the best containers to be opening in your raids")
+st.write("- Use the wallet to start a zero to hero run")
+st.write("- Contribute to the collection of container/loot data")
