10a69f3fcfa973080f913bde43f0f610749ddf60
huoates <huoates@gmail.com>
2026-05-17 18:59:19 -0500

    Add new models and fields for Genai integration and item management

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+from typing import List, Dict, Any, Optional
 
 
 class Zone(BaseModel):
@@ -22,10 +24,64 @@ class Container(BaseModel):
 class Item(BaseModel):
     model_config = ConfigDict(extra="ignore")
     id: int
+    created_at: datetime
+    dimension_x: int
+    dimension_y: int
     name: str
+    name_in_raid: str | None
     thumbnail_url: str | None
 
 
 class Opening(BaseModel):
     model_config = ConfigDict(extra="ignore")
     id: int
+    created_at: datetime
+    difficulty_id: int
+    zone_id: int
+    container_id: int
+    difficulty: Optional[Difficulty] = None
+    zone: Optional[Zone] = None
+    container: Optional[Container] = None
+
+
+class GenaiItemDetails(BaseModel):
+    quantity: int
+    item_name: str
+
+
+class GenaiOutputJson(BaseModel):
+    items: List[GenaiItemDetails]
+    container_name: str
+
+
+class AiAddLootSession(BaseModel):
+    model_config = ConfigDict(extra="ignore")
+    id: int
+    created_at: datetime
+    prompt_token_count: int
+    candidates_token_count: int
+    total_token_count: int
+    difficulty: Optional[Difficulty]
+    zone: Optional[Zone]
+
+
+class AiAddLoot(BaseModel):
+    model_config = ConfigDict(extra="ignore")
+    id: int
+    created_at: datetime
+    image_path: str
+    image_hash: str
+    genai_output_json: GenaiOutputJson
+    ai_add_loot_session: Optional[AiAddLootSession]
+    review_status: str
+
+
+class Loot(BaseModel):
+    model_config = ConfigDict(extra="ignore")
+    id: int
+    created_at: datetime
+    opening_id: int
+    quantity: int
+    item_id: int
+    opening: Optional[Opening] = None
+    item: Optional[Item] = None
