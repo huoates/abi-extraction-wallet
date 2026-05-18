8668d0879853a53fd7a35829d63387097f36d494
huoates <huoates@gmail.com>
2026-05-17 21:55:27 -0500

    Move most important chart up to the top for the loot charts page

--- pages/loot.py
+++ pages/loot.py
@@ -9,17 +9,16 @@ conn = st.connection(
     key=st.secrets["SUPABASE_KEY"],
 )
 
-response = conn.table("openings_by_container").select("*").execute()
-df = pd.DataFrame(response.data)
-# df["count"] = pd.to_numeric(df["count"])
-df = df.sort_values(by="count", ascending=False)
-st.write(df)
-st.title("Openings by Container")
-st.bar_chart(df, x="name", y="count")
-
 response = conn.table("koen_efficiency_by_container").select("*").execute()
 df_koen = pd.DataFrame(response.data)
 df_koen = df_koen.sort_values(by="eligible_koen_per_slot_per_opening", ascending=False)
 st.title("Average Koen per Slot per Opening by Container")
 st.write("This visual assumes you only pick up items worth at least $5000 per slot.")
 st.bar_chart(df_koen, x="container_name", y="eligible_koen_per_slot_per_opening")
+
+response = conn.table("openings_by_container").select("*").execute()
+df = pd.DataFrame(response.data)
+df = df.sort_values(by="count", ascending=False)
+st.write(df)
+st.title("Openings by Container")
+st.bar_chart(df, x="name", y="count")
